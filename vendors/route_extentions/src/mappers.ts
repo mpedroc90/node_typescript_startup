98c9a8f2c8cee15ba6026aa939f31f94846623ad
mpedroc90 <mpedroc90@gmail.com>
2019-09-23 12:06:37 -0400

    Refactor Mappers

--- vendors/route_extentions/src/mappers.ts
+++ vendors/route_extentions/src/mappers.ts
@@ -1,10 +1,10 @@
 import { ControllerRouted } from "./types";
 
 export function MapToControllersRouted<T> (controllers:T[]) : ControllerRouted[] {
-  return controllers as unknown as ControllerRouted[];
+  return controllers  as ControllerRouted[];
 }
 
 export function MapToControllerRouted<T> (controllers:T) : ControllerRouted {
-  return controllers as unknown as ControllerRouted;
+  return controllers as ControllerRouted;
 }
 
